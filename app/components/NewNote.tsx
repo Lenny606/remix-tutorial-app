cbc85b60bf1e20ddde5454151319afeb5dccb937
Tomas Kravcik <root@Laptop-i7>
2025-01-05 20:08:21 +0100

    fix form

--- app/components/NewNote.tsx
+++ app/components/NewNote.tsx
@@ -3,11 +3,12 @@ export default function NewNote() {
     return (
         <div className="p-4 bg-gray-100 rounded-md">
             <h2 className="text-xl font-semibold text-gray-700 mb-4">Create a New Note</h2>
-            <form className="space-y-4" method={'post'} id={'note-form'}>
+            <form className="space-y-4" method={'post'} id={'note-form'} >
                 <textarea
                     className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300"
                     placeholder="Write your note here..."
                     rows={5}
+                    name={'content'}
                 ></textarea>
                 <button
                     type="submit"
