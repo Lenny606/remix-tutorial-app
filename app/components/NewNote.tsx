45c6624fc587098bc7d19e6fd11326ac0e11645f
Tomas Kravcik <kravcik@procorp.cz>
2025-01-12 16:55:27 +0100

    use hooks

--- app/components/NewNote.tsx
+++ app/components/NewNote.tsx
@@ -1,19 +1,32 @@
-import {Form} from "@remix-run/react";
+import {Form, useActionData, useNavigation} from "@remix-run/react";
 
+type Response = {
+    message: string
+}
 export default function NewNote() {
+
+    const navigation = useNavigation()
+    const isSubmitting = navigation.state === "submitting"
+    const actionData : Response = useActionData();
     return (
         <div className="p-4 bg-gray-100 rounded-md">
             <h2 className="text-xl font-semibold text-gray-700 mb-4">Create a New Note</h2>
-            <Form className="space-y-4" method={'post'} id={'note-form'} >
+            <Form className="space-y-4" method={'post'} id={'note-form'}>
                 <textarea
                     className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300"
                     placeholder="Write your note here..."
                     rows={5}
                     name={'content'}
                 ></textarea>
+                {
+                    actionData.message && (
+                        <div className="text-green-800 text-center font-bold">{actionData.message}</div>
+                    )
+                }
                 <button
                     type="submit"
                     className="px-4 py-2 bg-blue-500 text-white font-medium rounded-md hover:bg-blue-600"
+                    disabled={isSubmitting}
                 >
                     Save Note
                 </button>
--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -3,22 +3,32 @@ import NewNote from "~/components/NewNote";
 import {redirect} from "@remix-run/node";
 import NoteList from "~/components/NoteList";
 import {dummyNotes} from "../../data/data";
-import {useLoaderData} from "@remix-run/react";
-type Note = {
+import {useLoaderData, useActionData} from "@remix-run/react";
 
+type Note = {
     title: string
     content: string
-
+}
+type Response = {
+    message: string
 }
 //compoent triggered by get request
 export default function NotesPage() {
-    //gets data from loader action
-    const data: Note[] = useLoaderData();
+    //gets data from loader / action  => accessible anywhere
+    const loaderData: Note[] = useLoaderData();
+    const actionData: Response = useActionData();
+
     return (
         <main className="max-w-2xl mx-auto mt-10 p-6 border border-gray-300 rounded-lg shadow-md bg-white">
             <h1 className="text-2xl font-bold text-center text-gray-800 mb-6">NotesPage</h1>
             <NewNote/>
-            <NoteList notes={data}/>
+            {
+                actionData.message && (
+                    <div className="text-green-800 text-center font-bold">{actionData.message}</div>
+                )
+            }
+
+            <NoteList notes={loaderData}/>
         </main>
     );
 }
@@ -33,22 +43,41 @@ export function links() {
 // BUILT IN METHODS FOR HTTP ---------------------------
 //trigger get request , rendered on server
 export async function loader() {
- //logic to get data ...
+    //logic to get data ...
     const data: Note[] = dummyNotes;
 
-    // to get data useLoader hook
+    if (data.length === 0) {
+        throw new Response({message: "empty data"}, {
+                status: 404
+            }  //uses Catch Boundary COmpoentn
+        )
+    }
+
+// to get data useLoader hook
     return data;
+}
+export function CatchBoundary(){
+        // const response = useCatch() //DEPRECATED>>
 }
 
 //function to handle ssr request in routes -waits for POST request
 export async function action(data: { request: Request }): Promise<Response> {
-    const { request } = data;
+    const {request} = data;
     const formData = await request.formData();
     const noteData = Object.fromEntries(formData) //posibble way
     // const note = {
     //     title: formData.get("title") || "",
     //     content: formData.get("content") || "",
     // }
+
+    if (true) {
+        //return test actionData
+
+        const error: Response = {
+            message: "Hello"
+        }
+        return error;
+    }
 
     // Use formData as needed
     console.log(noteData);
